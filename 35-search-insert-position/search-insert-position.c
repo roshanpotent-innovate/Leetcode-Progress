ff54e95465afd48ec490722ace00b3e5d098f2fd
Roshan V <mailtoroshn@gmail.com>
2026-08-03 20:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (94.41%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+    int mid;
+    while(left <= right){
+        mid = (left + right)/2;
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid] > target){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return left;
+}
