79db22a8b98de6ac998d8da7a8332513af6493bb
Roshan V <mailtoroshn@gmail.com>
2026-08-03 19:57:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (92.41%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
@@ -0,0 +1,12 @@
+int firstUniqChar(char* s) {
+    int count[26] = {0};
+    for(int i = 0; s[i] != '\0'; i++){
+        count[s[i] - 'a']++;
+    }
+    for(int i = 0; s[i] != '\0'; i++){
+        if(count[s[i] - 'a'] == 1){
+            return i;
+        }
+    }
+    return -1;
+}
