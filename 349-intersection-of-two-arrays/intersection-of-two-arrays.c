1a2ca61d343b33eefba83795c614f3a43ae405dc
Roshan V <mailtoroshn@gmail.com>
2026-09-07 22:49:44 +0530

    Time: 3 ms (38.13%) | Memory: 10.9 MB (41.49%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int *arr = malloc((nums1Size + nums2Size) * sizeof(int));
+    int k = 0;
+    for(int i = 0;i<nums1Size;i++){
+        int ax = 0;
+        for(int j = 0; j<k;j++){
+            if(arr[j] == nums1[i]){
+                ax = 1;
+                break;
+            }
+        }
+        if(ax){
+            continue;
+        }
+        for(int l = 0; l<nums2Size; l++){
+            if(nums1[i] == nums2[l]){
+                arr[k] = nums1[i];
+                k++;
+                break;
+            }
+        }
+    }
+    *returnSize = k;
+    return arr;
+}
