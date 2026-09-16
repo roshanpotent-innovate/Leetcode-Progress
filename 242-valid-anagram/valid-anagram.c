6942c8de5e15fba74b46b57e7dbe997e981cdd66
Roshan V <mailtoroshn@gmail.com>
2026-09-16 10:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (89.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.c
+++ 242-valid-anagram/valid-anagram.c
@@ -8,10 +8,10 @@ bool isAnagram(char* s, char* t) {
     }
     for(int i = 0; s[i] != '\0'; i++){
         count[s[i] - 'a']++;
-    }
-    for(int i = 0; t[i] != '\0'; i++){
         count[t[i] - 'a']--;
-        if(count[t[i] - 'a'] < 0){
+    }
+    for(int i = 0;i<26;i++){
+        if(count[i] != 0){
             return false;
         }
     }
